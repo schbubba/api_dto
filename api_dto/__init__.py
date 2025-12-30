bb106d093a41f558b9ad10072c9b3b80f7eb4142
James Hollon <james.hollon@gmail.com>
2025-12-30 09:51:41 -0600

    add json_field

--- api_dto/__init__.py
+++ api_dto/__init__.py
@@ -1,5 +1,6 @@
 from .base_dto import BaseDTO
-from .api_dto import api_dto
+from .api_dto import api_dto, json_field
 from .sensitive_fields import SensitiveFields, is_sensitive_field
 
-__all__ = [ "BaseDTO", "api_dto", "SensitiveFields", "is_sensitive_field" ]
+
+__all__ = [ "BaseDTO", "api_dto", "SensitiveFields", "is_sensitive_field", "json_field" ]
