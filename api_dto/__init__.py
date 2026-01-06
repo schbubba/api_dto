ee38d9e99b02a3edf7cfcae380a4a436cf659f0c
James Hollon <james.hollon@gmail.com>
2026-01-05 19:54:38 -0600

    fix init

--- api_dto/__init__.py
+++ api_dto/__init__.py
@@ -1,6 +1,6 @@
 from .base_dto import BaseDTO
-from .api_dto import api_dto, json_field
+from .api_dto import api_dto
 from .sensitive_fields import SensitiveFields, is_sensitive_field
 
 
-__all__ = [ "BaseDTO", "api_dto", "SensitiveFields", "is_sensitive_field", "json_field" ]
+__all__ = [ "BaseDTO", "api_dto", "SensitiveFields", "is_sensitive_field" ]
